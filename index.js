ea440128be86f3232c9f1b36c9ac6d6028c8a6a8
Arkadiy <frizzzbe@yandex.ru>
2023-04-28 16:04:41 +0300

    Include JWT + post query

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 import express from 'express';
+import jwt from 'jsonwebtoken'
 
 const app = express()
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!')
+})
+
+// POST http://localhost:4444/auth/login
+// По таком запросу будет дан ответ содержащий вложенный JSON
+app.post('/auth/login', (req, res) =>{
+  // req.body
+  const token = jwt.sign({
+    email: req.body.email,
+    fullname: "Петя Жабкин"
+  },
+  'code phrase')
+
+  res.json({
+    success: true,
+    count: 42,
+    token
+  })
 })
 
 app.listen(4444, (err) => {
