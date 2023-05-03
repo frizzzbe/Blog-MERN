69ceb734914ab765002db56b5b84a569548438cc
Arkadiy <frizzzbe@yandex.ru>
2023-05-03 17:10:36 +0300

    added object properties

--- index.js
+++ index.js
@@ -5,28 +5,38 @@ const app = express()
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!')
+  res.send('Hello World!')
+})
+
+app.get('/posts', (req, res) => {
+  res.send({
+    name: 'Alan',
+    surname: 'walker',
+    d_Size: '16sm'
+  })
 })
 
 // POST http://localhost:4444/auth/login
 // По таком запросу будет дан ответ содержащий вложенный JSON
 app.post('/auth/login', (req, res) =>{
-  // req.body
   const token = jwt.sign({
     email: req.body.email,
     fullname: "Петя Жабкин"
   },
-  'code phrase')
+  'кодовая фраза для шифровки сообщения')
 
+  // ответное сообщение
   res.json({
-    success: true,
+    success: false,
     count: 42,
+    simple: 'string',
     token
   })
 })
 
 app.listen(4444, (err) => {
   if (err) {
+    console.log('error ')
     throw console.error(err)
   }
   console.log('Server is OK!')
